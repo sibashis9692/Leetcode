e210d68402194dae955a168c667e6d62f4a3e4e8
Sibashis Nayak <90366636+sibashis9692@users.noreply.github.com>
2023-10-15 14:21:51 +0530

    Time: 3 ms (6.77%), Space: 42.4 MB (12.19%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 1;
+
+        while(0 < i && nums[i] <= nums[i - 1]){
+            i -= 1;
+        }
+
+
+        if(i <= 0){
+            Arrays.sort(nums);
+        }
+
+        else{
+            int small = nums[i];
+            int index = i - 1;
+
+
+            for(int j = i; j < nums.length; j++){
+                if(small >= nums[j] && nums[j] > nums[i - 1]){
+                    small = nums[j];
+                    index = j;
+                }
+            }
+
+            int data = nums[i - 1];
+            nums[i - 1] = nums[index];
+            nums[index] = data;
+
+            System.out.println(i);
+            Arrays.sort(nums, i, nums.length);
+        }
+    }
+}
